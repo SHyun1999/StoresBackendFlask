d064a73753feaefaf649980316552f109274ccf4
SHyun1999 <86813210+SHyun1999@users.noreply.github.com>
2024-03-14 14:19:56 -0600

    dummy endpoint

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+stores = [
+    {
+        "name": "my store",
+        "items": [
+            {
+                "name": "chair",
+                "price": 420.69
+            }
+        ]
+    }
+]
+
+@app.get("/store")
+def get_stores():
+    return {"stores": stores}
